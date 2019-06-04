11000d9ded7651cc10bb6605c24702979933282b
Tegar Imansyah <tegar@alterra.id>
2019-06-04 11:27:35 +0700

    initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = '@tegarimansyah'
+SITENAME = 'Tegar & Words'
+SITESUBTITLE = 'Technology, Lifestyle, Idea. Simply Written'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Jakarta'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Custom Header
+
+HOME_COVER = "assets/images/home.jpg"
+
+HEADER_COVERS_BY_TAG = {
+    'cupcake': 'assets/images/rainbow_cupcake_cover.png', 
+    'general':"assets/images/article_cover.jpg"
+}
+
+AUTHORS_BIO = {
+  "tegarimansyah": {
+    "name": "Tegar Imansyah",
+    "cover": "assets/images/japan-lake.jpeg",
+    "image": "assets/images/avatar.jpeg",
+    "website": "https://tegarimansyah.github.io",
+    "linkedin": "tegarimansyah",
+    "github": "tegarimansyah",
+    "location": "Indonesia",
+    "bio": 'Tegar interested in engineering topics such as Linux, Programming Language, and Embedded System (IoT). Gave some talks in local meet-up and international conference about technology.<br /> Any discussions are welcome. <a href="/pages/about" style="color: #3ea9dd;">>>More about me here <<</a>'
+  }
+}
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+THEME = 'attila'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+STATIC_PATHS = ['assets']
+
+### Theme specific settings
+COLOR_SCHEME_CSS = 'github.css'
+
+CSS_OVERRIDE = ['assets/css/myblog.css']
+
+### Plugins
+
+PLUGIN_PATHS = [
+  'pelican-plugins'
+]
+
+PLUGINS = [
+  'sitemap',
+  'neighbors',
+  'assets'
+]
